adc178c91ee8120fcb2e9c863aae159bc6577c8c
Flopiras <Flopiras@yahoo.it>
2023-11-27 12:52:54 +0100

    added exception

--- src/main/java/org/java/Calculator.java
+++ src/main/java/org/java/Calculator.java
@@ -44,7 +44,9 @@ public class Calculator {
 		return result;
 	}
 	
-	public float divide(float num1, float num2) {
+	public float divide(float num1, float num2) throws Exception {
+		
+		if(num2 == 0) throw new Exception();
 		
 		float result = num1 / num2;
 		
